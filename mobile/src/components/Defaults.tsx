26d9165c0ddcacb70ab7d5a0f2aa364e199b73c2
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-05-06 21:05:11 -0400

    chore: "Default" LegendList component

--- mobile/src/components/Defaults.tsx
+++ mobile/src/components/Defaults.tsx
@@ -1,5 +1,12 @@
+import type { LegendListProps, LegendListRef } from "@legendapp/list";
+import { LegendList as RawLegendList } from "@legendapp/list";
+import { AnimatedLegendList as RawAnimatedLegendList } from "@legendapp/list/reanimated";
 import type { FlashListProps } from "@shopify/flash-list";
 import { FlashList as SFlashList } from "@shopify/flash-list";
+import { cssInterop } from "nativewind";
+import type { ForwardedRef } from "react";
+import { forwardRef, useMemo, useRef } from "react";
+import { useTranslation } from "react-i18next";
 import type { FlatListProps, ScrollViewProps } from "react-native";
 import {
   FlatList as RNFlatList,
@@ -41,6 +48,68 @@ export function SheetsFlatList<TData>(props: FlatListProps<TData>) {
 /** `<FlashList />` from `react-native-actions-sheet` with some defaults applied. */
 export function SheetsFlashList<TData>(props: FlashListProps<TData>) {
   return <RNASFlashList {...ScrollablePresets} {...props} />;
+}
+//#endregion
+
+//#region Legend List
+type LegendListSignature = typeof RawLegendList;
+const WrappedLegendList = cssInterop(RawLegendList, {
+  className: "style",
+  contentContainerClassName: "contentContainerStyle",
+}) as LegendListSignature;
+
+type AnimatedLegendListSignature = typeof RawAnimatedLegendList;
+const WrappedAnimatedLegendList = cssInterop(RawAnimatedLegendList, {
+  className: "style",
+  contentContainerClassName: "contentContainerStyle",
+}) as AnimatedLegendListSignature;
+
+function LegendListImpl<T>(
+  {
+    extraData,
+    recycleItems = true,
+    animated,
+    ...props
+  }: LegendListProps<T> & { animated?: boolean },
+  ref?: ForwardedRef<LegendListRef>,
+) {
+  const { i18n } = useTranslation();
+
+  const dependencies = useMemo(
+    () => [i18n.language, extraData],
+    [i18n.language, extraData],
+  );
+
+  const ListComponent = useMemo(
+    () => (animated ? WrappedAnimatedLegendList : WrappedLegendList),
+    [animated],
+  );
+
+  return (
+    <ListComponent
+      // @ts-expect-error - Ref should be compatible with Animated Legend List.
+      ref={ref}
+      {...ScrollablePresets}
+      extraData={dependencies}
+      recycleItems={recycleItems}
+      {...props}
+    />
+  );
+}
+
+/** Legend List supporting NativeWind styles. */
+export const LegendList = forwardRef(LegendListImpl) as LegendListSignature;
+
+/** Animated Legend List supporting NativeWind styles. */
+export const AnimatedLegendList = forwardRef(
+  function AnimatedLegendList(props, ref) {
+    // @ts-expect-error - `animated` prop should be "hidden" from use.
+    return <LegendList ref={ref} {...props} animated />;
+  },
+) as AnimatedLegendListSignature;
+
+export function useLegendListRef() {
+  return useRef<LegendListRef>(null);
 }
 //#endregion
 
